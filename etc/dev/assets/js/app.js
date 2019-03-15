a8443e169d58def6c04b0a4e7c9aadb6a7aef55b
HawkinsZhao <hawkinszhao@outlook.com>
2019-03-15 14:30:28 +0800

    Add env key edit

--- etc/dev/assets/js/app.js
+++ etc/dev/assets/js/app.js
@@ -8,11 +8,20 @@ EnviromentsPool.prototype.render = function () {
         <td class="collapsing" onclick="onEnvDeleteClick(${idx})">
             <i class="trash icon red env-remove"></i>
         </td>
-        <td class="collapsing">${env.key}</td>
-        <td class="env-value" data-idx="${idx}" onclick="onEnvValueClick(${idx})">
+        <td class="collapsing env-key"
+            data-idx="${idx}"
+            onclick="onEnvItemClick('key', ${idx})">
+          <p class="key-content">${env.key}</p>
+          <div class="ui input hide env-input">
+              <input type="text" onblur="onEnvItemBlur('key', ${idx})">
+          </div>
+        </td>
+        <td class="env-value"
+            data-idx="${idx}"
+            onclick="onEnvItemClick('value', ${idx})">
             <p class="value-content">${env.value}</p>
-            <div class="ui input hide">
-                <input type="text" onblur="onEnvInputBlur(${idx})">
+            <div class="ui input hide env-input">
+                <input type="text" onblur="onEnvItemBlur('value', ${idx})">
             </div>
         </td>
     </tr>`
@@ -27,8 +36,8 @@ EnviromentsPool.prototype.push = function (key, value) {
   this.pool.push({ key, value })
 }
 
-EnviromentsPool.prototype.setValue = function (idx, value) {
-  this.pool[idx].value = value
+EnviromentsPool.prototype.set = function (key, idx, value) {
+  this.pool[idx][key] = value
 }
 
 EnviromentsPool.prototype.deleteAtIndex = function (index) {
