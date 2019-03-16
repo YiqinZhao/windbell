46e1e6c87c5dd86a51eabf4037065daed7b83c78
HawkinsZhao <hawkinszhao@outlook.com>
2019-03-16 13:51:19 +0800

    remove __version__

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from windbell import __version__
 from setuptools import setup, find_packages
 
 with open('README.md') as f:
@@ -12,7 +11,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='windbell',
-    version=__version__,
+    version='0.1.0',
     description='Fast email delivery in the wind.',
     long_description=readme,
     long_description_content_type='text/markdown',
