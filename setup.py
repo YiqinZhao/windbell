1818c94ddedb1716138ba343c2aa50e6a0cd605a
HawkinsZhao <hawkinszhao@outlook.com>
2019-03-16 14:02:19 +0800

    New version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='windbell',
-    version='0.1.0',
+    version='0.1.1',
     description='Fast email delivery in the wind.',
     long_description=readme,
     long_description_content_type='text/markdown',
